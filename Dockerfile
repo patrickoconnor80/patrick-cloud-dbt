93db3963e2171871d13bc627a44fe2fff7e8f142
Patrick O'Connor <patrickoconnor80@Patricks-MacBook-Pro-2.local>
2024-04-17 19:43:36 -0400

    update dbt-docs image

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM python:3.9
 
 # Install DBT Python Package
 RUN pip install dbt-core==1.7.11
+RUN pip install dbt-databricks==1.7.13
 
 # Copy all contents of repo into container
 RUN mkdir dbt
 COPY . dbt
+WORKDIR dbt
 
 # Prep for container execution
-ENTRYPOINT ["/bin/bash", "dbt/bin/entrypoint.sh"]
+ENTRYPOINT ["/bin/bash", "bin/entrypoint.sh"]
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cat > ~/.dbt/profiles.yml <<EOF
+cat > patrick_cloud/profiles.yml <<EOF
 patrick_cloud:
   target: dev
   outputs:
@@ -9,12 +9,12 @@ patrick_cloud:
       catalog: snowplow
       schema: atomic
       host: $DATABRICKS_HOST
-      http_path:$DATABRICKS_SQL_ENDPOINT
+      http_path: $DATABRICKS_SQL_ENDPOINT
       token: $DATABRICKS_TOKEN
       threads: 1
 EOF
 
-cd patrick-cloud
+cd patrick_cloud
 dbt deps
-dbt docs generate --target dev
-dbt docs serve
+dbt docs generate --target dev --profiles-dir .
+dbt docs serve --profiles-dir .
